68261fefb6da47e5f2336b87713bb59a20a4e469
Anderson Parra <ander.pp@gmail.com>
2015-08-10 23:29:25 +0100

    add posts aggregation by month

--- routes/posts.js
+++ routes/posts.js
@@ -5,10 +5,16 @@ var router = express.Router();
 
 router.get('/', function(req, res, next) {
   req.execute(function(db) {
-    db.collection('posts').find().toArray(function(error, posts) {
-      res.render('posts', { 'posts': posts });
-      db.close();
-    });
+    db.collection('posts').aggregate(
+      [
+        { $match: { published: true } },
+        { $group: { _id: { $dateToString: { format: "%m/%Y", date: "$date" } },
+           posts: { "$push": "$$ROOT" } }
+        }
+      ], function(error, posts) {
+        res.render('posts', { 'postsByMonth': posts });
+        db.close();
+      });
   });
 });
 
