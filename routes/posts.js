5bce7be4976c82fef52d18b6aa33f368d3fd2db3
eannpaa <anderson.parra.de.paula@ericsson.com>
2015-08-11 14:41:48 +0100

    create new post

--- routes/posts.js
+++ routes/posts.js
@@ -13,8 +13,28 @@ router.get('/', function(req, res, next) {
         }
       ], function(error, posts) {
         res.render('posts', { 'postsByMonth': posts });
-        db.close();
       });
+  });
+});
+
+router.get('/post', function(req, res, next) {
+  res.render('posts/new.jade');
+});
+
+router.post('/post', function(req, res, next) {
+  var post = {
+    title: req.body.title,
+    body: req.body.body,
+    author: req.body.author,
+    date: new Date(),
+    published: req.body.published === 'on'
+  };
+  
+  req.execute(function(db) {
+    db.collection('posts').insert(post, function(error, data) {
+      db.close();
+      res.redirect('/')
+    });
   });
 });
 
