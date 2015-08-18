95248de11388ac88c7d3a67e37fad75e955a6b05
eannpaa <anderson.parra.de.paula@ericsson.com>
2015-08-18 14:58:05 +0100

    change the app architecture

--- app.js
+++ app.js
@@ -1,72 +1,28 @@
 var express = require('express'),
     path = require('path'),
-    favicon = require('serve-favicon'),
-    logger = require('morgan'),
     cookieParser = require('cookie-parser'),
     bodyParser = require('body-parser'),
-    mongodb = require('mongodb'),
-    Q = require('q');
+    MongoClient = require('mongodb').MongoClient; 
 
-var posts = require('./routes/posts');
-var users = require('./routes/users');
+MongoClient.connect('mongodb://localhost:27017/labpad', function(error, db) {
+  if (error) throw error;
 
-var app = express();
+  var app = express();
+  var routes = require('./routes');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+  // view engine setup
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'jade');
 
-app.use(function(req, res, next) {
-  req.execute = function(promise) {
-    mongodb.MongoClient.connect('mongodb://localhost:27017/labpad', function(err, db) {
-      if(err) throw err;
-      promise(db); // close db into function :(
-    });
-  };
-  return next();
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(cookieParser());
+  app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+  app.use(express.static(path.join(__dirname, 'public')));  
+  
+  // application routes
+  routes(app, db);
+
+  app.listen(8080);
+  console.log('Express server listening on port 8080');
 });
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(require('stylus').middleware(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', posts);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
-module.exports = app;
